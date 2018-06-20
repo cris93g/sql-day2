60bc39946b0a8e5708ceb3d91006d48d8f77a355
cris93g <37388115+cris93g@users.noreply.github.com>
2018-06-20 12:45:19 -0500

    changed a few things

--- Chinook.js
+++ Chinook.js
@@ -2,3 +2,21 @@ SELECT *
 FROM Invoice i
 JOIN InvoiceLine il ON il.invoiceId = i.invoiceId
 WHERE il.UnitPrice>0.99;
+
+
+SELECT i.InvoiceDate, c.FirstName, c.LastName, i.Total
+FROM Invoice i
+JOIN Customer c ON i.CustomerId = c.CustomerId;
+
+SELECT c.FirstName, c.LastName, e.FirstName, e.LastName
+FROM Customer c
+JOIN Employee e ON c.SupportRepId = e.EmployeeId;
+
+SELECT al.Title,ar.Name
+FROM Album al
+JOIN Artist ar ON al.ArtistId = ar.ArtistId;
+
+SELECT pt.TrackId
+FROM PlayListTrack pt
+JOIN Playlist p ON p.PlaylistId= pt.PlaylistId
+WHERE p.Name ="Music"
